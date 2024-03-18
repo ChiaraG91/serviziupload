d563feab939b8ae7b68b53ec6135b479bcc49d4c
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-18 15:42:57 +0100

    Add files via upload

---
+++ servizi-fileupload/src/main/java/com/servizi/servizifileupload/ServiziFileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.servizi.servizifileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiziFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiziFileuploadApplication.class, args);
+	}
+
+}
---
+++ servizi-fileupload/src/test/java/com/servizi/servizifileupload/ServiziFileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servizi.servizifileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiziFileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
